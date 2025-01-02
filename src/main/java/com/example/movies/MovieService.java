487680b96611bae408137687cc2520d950686bd5
UBC Student <mfakhir@students.cs.ubc.ca>
2025-01-02 18:39:24 +0300

    initial commit

---
+++ src/main/java/com/example/movies/MovieService.java
@@ -0,0 +1,24 @@
+package com.example.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    // returns Optional because the MovieID being searched may not
+    // exist, so it must return null else in that case
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
